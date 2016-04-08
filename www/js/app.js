0670a93cd55e2d695d30cb9da1bf8029d3f3ff03
Jon Van Wagenen <jvanwag08@gmail.com>
2016-04-08 11:10:10 -0600

    changed the app name to instajam, added the scripts for the controllers

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,11 @@
-// Ionic Starter App
+// Ionic instajam App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'instajam' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+// 'instajam.services' is found in services.js
+// 'instajam.controllers' is found in controllers.js
+angular.module('instajam', ['ionic', 'instajam.controllers', 'instajam.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -77,6 +77,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         controller: 'AccountCtrl'
       }
     }
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'frLoginCtrl'
   });
 
   // if none of the above states are matched, use this as the fallback
