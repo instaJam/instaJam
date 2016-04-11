780ddc0e73ca280cd6e2a68ccae424065f1e31f2
Mike <mikeytown19@gmail.com>
2016-04-11 09:40:23 -0600

    message

--- www/js/controllers/frLoginCtrl.js
+++ www/js/controllers/frLoginCtrl.js
@@ -1,11 +1,21 @@
-angular.module('instajam').controller('frLoginCtrl', function($scope, $auth){
+angular.module('instajam').controller('frLoginCtrl', function($scope, $auth,$state){
 
-    $scope.login = function(user) {
-        $auth.login(user);
+    $scope.login = function(user){
+        $auth.login(user).then(function(res){
+            $state.go('tab.dash');
+        });
     }
 
     $scope.signUp = function(user) {
-        $auth.signup(user);
+        $auth.signup(user).then(function(response) {
+             $auth.setToken(response.data.token);
+             $state.go('tab.dash');
+        });
+    }
+    $scope.logout = function(){
+        $auth.logout().then(function(res){
+            $state.go('login');
+        });
     }
 
 })
--- www/js/controllers/frUserProfileCtrl.js
+++ www/js/controllers/frUserProfileCtrl.js
@@ -1,3 +1,21 @@
-angular.module('instajam').controller('frUserProfileCtrl', function($scope){
+angular.module('instajam').controller('frUserProfileCtrl', function($scope,$state,$auth, userService){
+
+    userService.getCurrentUser().then(function(res){
+                $scope.currentUser = res.data;
+            })
+
+
+    $scope.logout = function(){
+        $auth.logout().then(function(res){
+            $state.go('login');
+        });
+    }
+
+
+
+
+
+
+
 
 })
