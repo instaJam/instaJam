5d06d494b6a88d9ad618d6e0434ad3c99dc0ef8e
Breiden Busch <breiden.busch@gmail.com>
2016-04-11 09:39:11 -0600

    Making Changes

--- www/js/controllers/frLoginCtrl.js
+++ www/js/controllers/frLoginCtrl.js
@@ -1,11 +1,15 @@
-angular.module('instajam').controller('frLoginCtrl', function($scope, $auth){
+angular.module('instajam').controller('frLoginCtrl', function($scope, $auth, $state){
 
     $scope.login = function(user) {
         $auth.login(user);
     }
 
     $scope.signUp = function(user) {
-        $auth.signup(user);
+        $auth.signup(user).then(function(response) {
+            console.log(response);
+            $auth.setToken(response.data.token);
+            $state.go('tab.dash');
+        })
     }
 
 })
--- www/js/controllers/frUserProfileCtrl.js
+++ www/js/controllers/frUserProfileCtrl.js
@@ -1,3 +1,14 @@
-angular.module('instajam').controller('frUserProfileCtrl', function($scope){
+angular.module('instajam').controller('frUserProfileCtrl', function($scope, $http){
+
+$scope.getCurrent = function() {
+    return $http({
+        method: 'GET',
+        url: '/api/me'
+    }).then(function(response) {
+        $scope.currentUser = response.data;
+    })
+}
+
+$scope.getCurrent();
 
 })
