00aa552c247aeaf0d6c285ea5d4fea6f1b773e89
Jon Van Wagenen <jvanwag08@gmail.com>
2016-04-14 11:22:04 -0600

    finished edit profile page

--- www/js/controllers/frUserProfileCtrl.js
+++ www/js/controllers/frUserProfileCtrl.js
@@ -66,16 +66,16 @@ angular.module('instajam').controller('frUserProfileCtrl', function($scope,$stat
         var b = document.getElementById('emailLabel')
         var f = document.getElementById('bioChange');
         var e = document.getElementById('bioLabel')
-        var g = document.getElementById('passwordChange');
-        var h = document.getElementById('passwordLabel')
+        // var g = document.getElementById('passwordChange');
+        // var h = document.getElementById('passwordLabel')
         a.style.display="block";
         b.style.display="block";
         e.style.display="block";
-        h.style.display="block";
+        // h.style.display="block";
         c.style.display="none";
         d.style.display="none";
         f.style.display="none";
-        g.style.display="none";
+        // g.style.display="none";
     }
 
 })
