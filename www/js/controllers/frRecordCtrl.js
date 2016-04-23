2530dca5e1b13b63c1bb6a0d3f66433b6ad51c34
berserkernosaj <nosajmahdlo@gmail.com>
2016-04-23 16:35:46 -0600

    record view improved, but not done yet

--- www/js/controllers/frRecordCtrl.js
+++ www/js/controllers/frRecordCtrl.js
@@ -3,10 +3,23 @@ angular.module('instajam').controller('frRecordCtrl', function($scope, $state, $
   userService.getCurrentUser().then(function(data){
       $scope.profile = data.data;
   });
+$scope.showYoutube = false;
 
+$scope.youtubeShower = function() {
+    $scope.showYoutube = !$scope.showYoutube;
+}
+$scope.showPicture = false;
+
+$scope.pictureShower = function() {
+    $scope.showPicture = !$scope.showPicture;
+}
 
 $scope.postVid = function(data, user){
   postService.postContent(data, user).then(function(res){
+  $scope.youtubeShower();
+  $scope.search_query.setPristine();
+  $scope.postInput.setPristine();
+
 })
 }
 
